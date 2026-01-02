7b2aa199b5e120e6f434f68a59d56caaa5a7f6e1
Lokesh <100463417+Luckyrag@users.noreply.github.com>
2026-01-02 23:42:15 +0530

    [Lokesh]: Console-Based BankApp Ref

---
+++ miniProject_1/BankOperation.java
@@ -0,0 +1,8 @@
+package miniProject_1;
+
+public interface BankOperation {
+		void deposit(double amount);
+	    void withdraw(double amount) throws Exception;
+	    double getBalance();
+	    void displayAccountDetails();
+}
---
+++ miniProject_1/SavingsAccount.java
@@ -0,0 +1,13 @@
+package miniProject_1;
+
+public class SavingsAccount extends BankAccount{
+	 public SavingsAccount(int accNo, String name, double balance) {
+	        super(accNo, name, balance);
+	    }
+	 @Override
+	    public void calculateInterest() {
+	        balance += balance * 0.04;
+	    }
+}
+
+
