14acb81a7d89fc1c0ae959e90984ee69723837e1
Lokesh <100463417+Luckyrag@users.noreply.github.com>
2026-01-06 23:38:41 +0530

    [Lokesh]: added JDBC Connection Task

---
+++ assignment_4/JDBC_Welcome.java
@@ -0,0 +1,30 @@
+package assignment_4;
+import java.sql.*;
+
+
+/*
+ * Established the connection with mySql.
+ * First Step 
+ */
+
+
+public class JDBC_Welcome {
+
+	public static void main(String[] args){
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/AdvanceJava","root","Lokesh@143");
+			System.out.println("Connection Created Successfully !");
+			con.close();
+		}
+		catch(ClassNotFoundException e) {
+			System.out.println("Class Not Found Error !");
+		}
+		catch(SQLException e) {
+			System.out.println("Connection not created !");
+		}
+		
+
+	}
+
+}
